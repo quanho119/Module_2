3ce15e485bb721305dd0b4541f3ed34cb9a6bf9f
MinhWaan <162463827+quanho119@users.noreply.github.com>
2024-05-27 19:11:04 +0700

    Tạo stack từ linked list

---
+++ StackAndQueue/Baitap/StackByLinkedList/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        stackOfIStrings();
+    }
+}
