396d6b4a42d279c0834fb27df8f098dc7ce1c30c
MinhWaan <162463827+quanho119@users.noreply.github.com>
2024-06-14 19:38:39 +0700

    bai Tap

--- StringAndRegex/baiTap/email/src/EmailExample.java
+++ StringAndRegex/baiTap/email/src/EmailExample.java
@@ -1,18 +1,10 @@
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailExample {
-    private static Pattern pattern;
-    private Matcher matcher;
-
-    private static final String EMAIL_REGEX =   "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
-
-    public EmailExample() {
-        pattern = Pattern.compile(EMAIL_REGEX);
-    }
+    private static final String pattern="[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)";
+    public EmailExample() {}
 
     public boolean checkEmail(String regex) {
-        matcher = pattern.matcher(regex);
-        return matcher.matches();
+        return Pattern.matches(pattern,regex);
     }
 }
